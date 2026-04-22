5e211c5433e2685adf482209a6345a2f5195a4ab
christel jean pablo <christeljeanpablo13@gmail.com>
2026-04-22 14:27:52 +0800

    Initial project setup with HTML, CSS, and JS structure- pablo

---
+++ FinalProject/physics_Sandbox/script.js
@@ -0,0 +1,10 @@
+/*
+=====================================================
+🎮 Physics Sandbox Game
+📘 Programming 2 Final Project
+
+📌 Description:
+Contains the main game loop, state management,
+and rendering logic.
+=====================================================
+*/
