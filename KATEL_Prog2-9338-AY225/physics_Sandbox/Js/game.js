c41553d848153ff6d31480c09db0ceed1cc72dd3
christel jean pablo <christeljeanpablo13@gmail.com>
2026-04-28 14:12:45 +0800

    Added background grid and goal visual improvements-dig

--- KATEL_Prog2-9338-AY225/physics_Sandbox/Js/game.js
+++ KATEL_Prog2-9338-AY225/physics_Sandbox/Js/game.js
@@ -29,17 +29,35 @@ function updateGame() {
 }
 
 function drawGame() {
-    ctx.clearRect(0,0,800,500);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Goal
+    // 🌐 background grid (NEW)
+    ctx.strokeStyle = "rgba(255,255,255,0.03)";
+    for (let x = 0; x < canvas.width; x += 40) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
+    }
+
+    for (let y = 0; y < canvas.height; y += 40) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
+        ctx.stroke();
+    }
+
+    // 🎯 goal
     ctx.fillStyle = "#22c55e";
     ctx.fillRect(goal.x, goal.y, goal.width, goal.height);
 
     if (goal.glow > 0) {
-        ctx.fillStyle = "rgba(34,197,94,0.3)";
-        ctx.fillRect(goal.x-5, goal.y-5, goal.width+10, goal.height+10);
+        ctx.fillStyle = `rgba(34,197,94,${goal.glow / 20})`;
+        ctx.fillRect(goal.x - 5, goal.y - 5, goal.width + 10, goal.height + 10);
     }
 
     objects.forEach(o => o.draw(ctx));
     particles.forEach(p => p.draw(ctx));
 }
+
+ 
