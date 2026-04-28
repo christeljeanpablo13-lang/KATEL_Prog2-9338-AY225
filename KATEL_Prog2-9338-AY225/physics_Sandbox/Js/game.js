5c71ad47829a0324c875f4ae8410a2e79013ac8f
christel jean pablo <christeljeanpablo13@gmail.com>
2026-04-28 15:01:16 +0800

    Converted drawing path into physics object-pablo

--- KATEL_Prog2-9338-AY225/physics_Sandbox/Js/game.js
+++ KATEL_Prog2-9338-AY225/physics_Sandbox/Js/game.js
@@ -31,33 +31,20 @@ function updateGame() {
 function drawGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 🌐 background grid (NEW)
-    ctx.strokeStyle = "rgba(255,255,255,0.03)";
-    for (let x = 0; x < canvas.width; x += 40) {
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvas.height);
-        ctx.stroke();
-    }
-
-    for (let y = 0; y < canvas.height; y += 40) {
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(canvas.width, y);
-        ctx.stroke();
-    }
-
-    // 🎯 goal
-    ctx.fillStyle = "#22c55e";
-    ctx.fillRect(goal.x, goal.y, goal.width, goal.height);
-
-    if (goal.glow > 0) {
-        ctx.fillStyle = `rgba(34,197,94,${goal.glow / 20})`;
-        ctx.fillRect(goal.x - 5, goal.y - 5, goal.width + 10, goal.height + 10);
-    }
-
+    // draw existing objects
     objects.forEach(o => o.draw(ctx));
-    particles.forEach(p => p.draw(ctx));
-}
 
- 
+    // 🖊️ DRAW PREVIEW (NEW)
+    if (isDrawing && currentPath.length > 1) {
+        ctx.beginPath();
+        ctx.moveTo(currentPath[0].x, currentPath[0].y);
+
+        for (let i = 1; i < currentPath.length; i++) {
+            ctx.lineTo(currentPath[i].x, currentPath[i].y);
+        }
+
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
+}
